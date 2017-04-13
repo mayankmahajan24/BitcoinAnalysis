5e9a6ae1ec8fe39fca28e608b27e84b7da1b9ba2
Mayank Mahajan <mayank.mahajan@gmail.com>
2017-04-12 22:23:17 -0400

    saving compressed matrix instead

--- analysis.py
+++ analysis.py
@@ -1,19 +1,28 @@
 import numpy as np
 from scipy.sparse import csr_matrix
 
+def save_sparse_csr(filename,array):
+    np.savez(filename,data = array.data ,indices=array.indices,
+             indptr =array.indptr, shape=array.shape )
+
+def load_sparse_csr(filename):
+    loader = np.load(filename)
+    return csr_matrix((  loader['data'], loader['indices'], loader['indptr']),
+                         shape = loader['shape'])
 
 def main():
 	train = np.loadtxt("txTripletsCounts.txt")
 	test = np.loadtxt("testTriplets.txt")
-	M = max(train[:,1])
-	N = max(train[:,0])
+	M = int(max(train[:,1]))
+	N = int(max(train[:,0]))
 
-	full_counts = np.zeros((N,M), dtype=np.int)
-
-	for row in train:
+	full_counts = np.zeros((N+1,M+1), dtype=np.int)
+	for row in train[:100,:]:
 		full_counts[row[0], row[1]] = row[2]
-
 	counts = csr_matrix(full_counts)
+
+	save_sparse_csr("counts",counts)
+
 
 if __name__ == "__main__":
 	main()
